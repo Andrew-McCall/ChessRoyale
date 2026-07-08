490edb869227a9b986a9d4a6bc73b86af07dbe94
Andrew-McCall <andrewdavidmccall@hotmail.com>
2026-07-08 10:51:47 +0100

    tailwind

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-	<h1>Chess Royale</h1>
+	<div class="min-h-screen flex items-center justify-center">
+		<h1 class="text-4xl font-bold text-amber-600">
+			Chess Royale
+		</h1>
+	</div>
 </template>
